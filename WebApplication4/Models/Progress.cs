0529a702083fc6546c2f2a413eecb84e4e460779
Stefan <stefan.labuschagne@gmail.com>
2017-08-26 10:55:12 +0200

    Add files via upload

---
+++ WebApplication4/Models/Progress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication4.Models
+{
+    public class Progress
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public String ID { get; set; }
+        public String Guid { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Description { get; set; }
+
+    }
+}
